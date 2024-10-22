07dae28e29fd5a3c0427af03cf28cd0f79887b30
Leonard <leonardmichaelmasters@hotmail.com>
2024-10-22 18:41:39 +0100

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import time
+
 binary_number = input("Please input a binary number: ")
 decimal_number = int(binary_number,2)
 print(decimal_number)
+
+
+def decimal_to_binary(num):
+    if num >= 1:
+        decimal_to_binary(num // 2)
+    print(num % 2, end='')
+
+decimal_to_binary(num=int(input("Please input a number: ")))
